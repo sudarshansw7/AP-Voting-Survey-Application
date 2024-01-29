6f271dc4394bd165630fd99dd015bb4aacaeb74b
sudarshansw7 <142533426+sudarshansw7@users.noreply.github.com>
2024-01-29 12:10:00 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+
+module.exports = {
+    database: {
+        host: 'localhost',
+        port: 3306,
+        user: 'username',
+        password: 'password',
+        database: 'dbname'
+    }
+};
