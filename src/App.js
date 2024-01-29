9bc1af9872348ba16ed1c59626484ec7afbd6079
sudarshansw7 <142533426+sudarshansw7@users.noreply.github.com>
2024-01-29 12:57:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+// Importing the config.js file
+import config from "./config";
 import Router from "./routes";
 import ThemeProvider from "./theme";
 import SnackBar from "./components/SnackBar";
 
 function App() {
+  // Accessing the database configuration
+  const { host, port, user, password, database } = config.database;
+
+  // Now you can use these values to connect to your database
+
   return (
     <ThemeProvider>
       <SnackBar />
@@ -12,3 +19,4 @@ function App() {
 }
 
 export default App;
+
